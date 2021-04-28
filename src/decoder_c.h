ac7adca07e93815b55ece4b7d7dc142d5a23ddc1
Autokaka <luao@bilibili.com>
2021-04-28 14:00:45 +0800

    del: unused header reference.

--- src/decoder_c.h
+++ src/decoder_c.h
@@ -1,12 +1,6 @@
 #pragma once
 
-#include "log.h"
 #include "player.h"
 #include "plugin/plugin_android.h"
-
-extern "C" {
-#include <libavformat/avformat.h>
-#include <libavutil/imgutils.h>
-}
 
 void* _decode(void*);
