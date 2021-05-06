27f4174486aed1c6133f2470ad0d9e18f9b746b9
Autokaka <luao@bilibili.com>
2021-05-06 12:18:31 +0800

    revert: entry page

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: VideoTestPage(),
+      home: EntryPage(),
     );
   }
 }
