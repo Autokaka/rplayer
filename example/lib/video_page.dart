2a80e84fc8aceae91376e18ddf03729bddbef3f1
Autokaka <luao@bilibili.com>
2021-05-11 13:31:11 +0800

    feat: update example

--- example/lib/video_page.dart
+++ example/lib/video_page.dart
@@ -13,17 +13,17 @@ class _VideoTestPageState extends State<VideoTestPage> {
       ..retryTimesOnDisconnect = 10
       ..retryDelayInMilliseconds = 2000,
   );
-  final player2 = RPlayer(
-    config: RPlayerConfig()
-      ..retryTimesOnDisconnect = 10
-      ..retryDelayInMilliseconds = 2000,
-  );
+  // final player2 = RPlayer(
+  //   config: RPlayerConfig()
+  //     ..retryTimesOnDisconnect = 10
+  //     ..retryDelayInMilliseconds = 2000,
+  // );
 
   @override
   void initState() {
     super.initState();
     player.startPlayWhenReady('rtsp://admin:admin@192.168.1.104:8554/live');
-    player2.startPlayWhenReady('rtsp://admin:admin@192.168.1.104:8554/live');
+    // player2.startPlayWhenReady('rtsp://admin:admin@192.168.1.104:8554/live');
   }
 
   @override
@@ -32,31 +32,33 @@ class _VideoTestPageState extends State<VideoTestPage> {
       appBar: AppBar(
         title: const Text('RPlayer Video Test Page'),
       ),
-      body: Flex(
-        direction: (MediaQuery.of(context).size.width >
-                MediaQuery.of(context).size.height)
-            ? Axis.horizontal
-            : Axis.vertical,
-        children: [
-          Expanded(
-            child: DPadDetector(
-              child: RPlayerView(
-                player: player,
-                width: MediaQuery.of(context).size.width,
-                height: 300,
+      body: Center(
+        child: Flex(
+          direction: (MediaQuery.of(context).size.width >
+                  MediaQuery.of(context).size.height)
+              ? Axis.horizontal
+              : Axis.vertical,
+          children: [
+            Expanded(
+              child: DPadDetector(
+                child: RPlayerView(
+                  player: player,
+                  width: MediaQuery.of(context).size.width,
+                  height: 300,
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: DPadDetector(
-              child: RPlayerView(
-                player: player2,
-                width: MediaQuery.of(context).size.width,
-                height: 300,
-              ),
-            ),
-          ),
-        ],
+            // Expanded(
+            //   child: DPadDetector(
+            //     child: RPlayerView(
+            //       player: player2,
+            //       width: MediaQuery.of(context).size.width,
+            //       height: 300,
+            //     ),
+            //   ),
+            // ),
+          ],
+        ),
       ),
     );
   }
